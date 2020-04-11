d7c31c35c1a0c044ced7d1c5c2974f2475840d3e
fujisanncode <812542982@qq.com>
2020-04-11 11:16:58 +0800

    add xterm

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,12 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src='./hello.css'></style>
+
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <el-button>shell console</el-button>
+    <div id="xterm" class="xterm" />
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+<script src='./hello.js'></script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/hello.js
@@ -0,0 +1,59 @@
+import 'xterm/css/xterm.css'
+import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit'
+import { AttachAddon } from 'xterm-addon-attach'
+
+export default {
+  name: 'hello',
+  props: {
+    socketURI: {
+      type: String,
+      default: 'ws://localhost:8078'
+    },
+  },
+  mounted () {
+    this.initSocket()
+  },
+  beforeDestroy () {
+    this.socket.close()
+    this.term.dispose()
+  },
+  methods: {
+    initTerm () {
+      const term = new Terminal({
+        fontSize: 14,
+        cursorBlink: true
+      });
+      const attachAddon = new AttachAddon(this.socket);
+      const fitAddon = new FitAddon();
+      term.loadAddon(attachAddon);
+      term.loadAddon(fitAddon);
+      term.open(document.getElementById('xterm'));
+      fitAddon.fit();
+      term.focus();
+      this.term = term
+    },
+    initSocket () {
+      this.socket = new WebSocket(this.socketURI);
+      this.socketOnClose();
+      this.socketOnOpen();
+      this.socketOnError();
+    },
+    socketOnOpen () {
+      this.socket.onopen = () => {
+        // 链接成功后
+        this.initTerm()
+      }
+    },
+    socketOnClose () {
+      this.socket.onclose = () => {
+        // console.log('close socket')
+      }
+    },
+    socketOnError () {
+      this.socket.onerror = () => {
+        // console.log('socket 链接失败')
+      }
+    }
+  }
+}
